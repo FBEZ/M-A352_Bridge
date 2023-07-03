9942441eac6a412c4c7372fed04721d7469b43b2
fbez <francesco.bez@gmail.com>
2023-07-03 15:34:32 +0200

    first commit

---
+++ components/M-A352/M-A352.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "M-A352.h"
+
+void hello(void)
+{
+    printf("Hello world\n");
+
+}
---
+++ main/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "M-A352.h"
+
+void app_main(void)
+{
+
+    const int uart_num = UART_NUM_2;
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    // Configure UART parameters
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, 5, 18, -1, -1));
+
+    printf("Ciao mondo\n");
+
+}
